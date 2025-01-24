74b1e3c35aa8d27d8f6830eaf458ac69a210643b
LosAllen <lincolnjallen@gmail.com>
2025-01-24 14:45:48 -0600

    finished

--- backend/controllers/contacts.js
+++ backend/controllers/contacts.js
@@ -2,71 +2,106 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+  try {
+    const db = mongodb.getDb();
+    const result = await db.collection('contacts').find().toArray();
+
+    res.status(200).json(result);
+  } catch (err) {
+    console.error('Error fetching contacts:', err);
+    res.status(500).json({ message: 'Error fetching contacts' });
+  }
 };
+
 
 const getSingle = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const db = mongodb.getDb();
+    const userId = new ObjectId(req.params.id);
+    const result = await db.collection('contacts').findOne({ _id: userId });
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: 'Contact not found' });
+    }
+  } catch (err) {
+    console.error('Error fetching contact:', err);
+    res.status(500).json({ message: 'Error fetching contact' });
+  }
 };
+
 
 const createContact = async (req, res) => {
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  try {
+    const db = mongodb.getDb();
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday,
+    };
+    const response = await db.collection('contacts').insertOne(contact);
+
+    if (response.acknowledged) {
+      res.status(201).json({ message: 'Contact created successfully!', id: response.insertedId });
+    } else {
+      res.status(500).json({ message: 'Failed to create contact.' });
+    }
+  } catch (err) {
+    console.error('Error creating contact:', err);
+    res.status(500).json({ message: 'An error occurred while creating the contact.' });
   }
 };
+
 
 const updateContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  // be aware of updateOne if you only want to update specific fields
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .replaceOne({ _id: userId }, contact);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  try {
+    const db = mongodb.getDb();
+    const contactId = new ObjectId(req.params.id);
+    const updatedContact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday,
+    };
+
+    const response = await db
+      .collection('contacts')
+      .updateOne({ _id: contactId }, { $set: updatedContact });
+
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'No contact found to update.' });
+    }
+  } catch (err) {
+    console.error('Error updating contact:', err);
+    res.status(500).json({ message: 'An error occurred while updating the contact.' });
   }
 };
 
+
 const deleteContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
-  console.log(response);
-  if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  try {
+    const db = mongodb.getDb();
+    const userId = new ObjectId(req.params.id);
+
+    const response = await db.collection('contacts').deleteOne({ _id: userId });
+
+    if (response.deletedCount > 0) {
+      res.status(200).json({ message: 'Contact deleted successfully!' });
+    } else {
+      res.status(404).json({ message: 'No contact found to delete.' });
+    }
+  } catch (err) {
+    console.error('Error deleting contact:', err);
+    res.status(500).json({ message: 'An error occurred while deleting the contact.' });
   }
 };
+
 
 module.exports = {
   getAll,
--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,32 +1,37 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 
 let _db;
 
 const initDb = (callback) => {
   if (_db) {
-    console.log('Db is already initialized!');
+    console.log('Database is already initialized!');
     return callback(null, _db);
   }
-  MongoClient.connect(process.env.MONGODB_URI)
+
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    return callback(new Error('MONGODB_URI is not defined.'));
+  }
+
+  MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((client) => {
-      _db = client;
+      _db = client.db(); // Use the default database from the connection string
+      console.log('Connected to MongoDB!');
       callback(null, _db);
     })
     .catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
       callback(err);
     });
 };
 
 const getDb = () => {
   if (!_db) {
-    throw Error('Db not initialized');
+    throw new Error('Database not initialized');
   }
-  return _db;
+  return _db; // Return the database object instead of the client
 };
 
-module.exports = {
-  initDb,
-  getDb
-};
+module.exports = { initDb, getDb };
