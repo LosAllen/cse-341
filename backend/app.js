21e8c486d63e223097d63198c8d35c001e8baed7
LosAllen <lincolnjallen@gmail.com>
2025-01-31 16:23:18 -0600

    fixed render

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+const port = process.env.PORT || 8080;
 
 app
   .use(bodyParser.json())
